fc47caca4ca81fcd19d3604bddb336360fbf45cc
Jonas-Kodehode <152984463+Jonas-Kodehode@users.noreply.github.com>
2024-04-12 00:21:56 +0200

    some more styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Display from "./components/Display";
 import Keypad from "./components/Keypad";
 import Arithmetics from "./components/Arithmetics";
+import styles from "./App.module.css"
 
 
 
@@ -11,11 +12,12 @@ function App() {
     const [result, setResult] = useState("")
 
     return (
-        <>
+        <div className={styles.fullPage}>
+        <h2 className={styles.calcText}>calc</h2>
             <Display input={input} result={result}/>
             <Arithmetics input={input} calculate={calculate} setCalculate={setCalculate} setResult={setResult}/>
             <Keypad input={input} setInput={setInput} setCalculate={setCalculate} setResult={setResult} />
-        </>
+        </div>
     );
 }
 
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,11 @@
-
+import styles from "./Display.module.css"
 
 export default function Display({input, result}) {
     
     return (
-        <div>
-            <h1>{input}</h1>
-            <h2>{result}</h2>
+        <div className={styles.display}>
+            <h1 className={styles.input}>{input}</h1>
+            <h2 className={styles.result}>{result}</h2>
         </div>
     )
 }
